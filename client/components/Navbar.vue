260c68fd9ad77a5236e62f5e792c69e2fb86bfde
Christabella Irwanto <christabella.irwanto@gmail.com>
2017-07-11 06:17:36 +0800

    modularize Navbar

---
+++ client/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="navbar">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">
+        <span class="icon is-large">
+          <i class="fa fa-newspaper-o" aria-hidden="true"></i>
+        </span>
+        <span class="navbar-item title" href="/">
+          Simple HN Clone
+        </span>
+      </a>
+      <div class="navbar-burger burger" data-target="navMenu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+
+    <div id="navMenu" class="navbar-menu">
+      <div class="navbar-end">
+        
+        <div class="navbar-item">
+          <p class="control">
+            <a class="button is-primary" href="https://github.com/jgthms/bulma/archive/0.4.3.zip">
+              <span class="icon">
+                <i class="fa fa-pencil-square-o"></i>
+              </span>
+              <span>Submit topic</span>
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+    </div>
+  </nav>
+</template>
+
+<style>
+</style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,39 +1,6 @@
 <template>
   <section class="container is-fluid">
-    <nav class="navbar">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="/">
-          <span class="icon is-large">
-            <i class="fa fa-newspaper-o" aria-hidden="true"></i>
-          </span>
-          <span class="navbar-item title" href="/">
-            Simple HN Clone
-          </span>
-        </a>
-        <div class="navbar-burger burger" data-target="navMenu">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
-
-      <div id="navMenu" class="navbar-menu">
-        <div class="navbar-end">
-          
-          <div class="navbar-item">
-            <p class="control">
-              <a class="button is-primary" href="https://github.com/jgthms/bulma/archive/0.4.3.zip">
-                <span class="icon">
-                  <i class="fa fa-pencil-square-o"></i>
-                </span>
-                <span>Submit topic</span>
-              </a>
-            </p>
-          </div>
-        </div>
-      </div>
-      </div>
-    </nav>
+    <Navbar/>
 
     <div class="tile is-vertical topics-container">
       <div v-for="topic in topics" class="box topic is-fullwidth">
@@ -71,9 +38,10 @@
 
 <script>
  import axios from 'axios'
-
+ import Navbar from '~/components/Navbar.vue'
  export default {
    components: {
+     Navbar
    },
    async asyncData () {
      let { data } = await axios.get('http://localhost:8080/topics')
